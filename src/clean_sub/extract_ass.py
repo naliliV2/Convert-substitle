374fbccd2b98d2a852ff24a02c3a853ce89366ed
naliliV2 <nalili0000007@gmail.com>
2022-04-20 17:39:02 +0200

    :heavy_plus_sign: Create extract_ass.py

---
+++ src/clean_sub/extract_ass.py
@@ -0,0 +1,85 @@
+#Function open a file and store the data in a list of lists in utf-8
+def read_file(file_name):
+    with open(file_name, 'r', encoding='utf-8') as f:
+        data = f.readlines()
+    return data
+
+#Function to clean the data (remove the \n and \ufeff and empty lines)
+def clean_data(data):
+    clean_data = []
+    for line in data:
+        line = line.strip("\n")
+        line = line.strip("\ufeff")
+        if line != '':
+            clean_data.append(line)
+    return clean_data
+
+ 
+#Function to detect [Aegisub Project Garbage] and delete all the lines between while [Events]
+def delete_garbage(data):
+    garbage = False
+    for i in range(len(data)):
+        if data[i] == '[Script Info]':
+            garbage = True
+        elif data[i] == '[Events]':
+            garbage = False
+            
+        if garbage == True:
+            data[i] = ''
+    return data
+
+#Function that detects the first word in the line "Format:" and stores all words in a list 
+def format_ass(data):
+    format_list = []
+    for i in range(len(data)):
+        if data[i].startswith('Format:'):
+            format_list = data[i].split(',')
+            format_list.pop(0)
+            format_list = [x.strip() for x in format_list]
+    return format_list
+
+#Function that receives a line and removes every character between { and }.
+def remove_brackets(line):
+    #transform line in a list
+    line = list(line)
+    garbage = False
+
+    for i in range(len(line)):
+        if line[i] == '{':
+            garbage = True
+        elif line[i] == '}':
+            line[i] = ' '
+            garbage = False
+            
+        if garbage == True:
+            line[i] = ' '
+    #retransform line in a str
+    line = ''.join(line)
+    
+    return line
+
+#Function that stores each segment (segments are separated by a ',') in a dictionary (each name is defined in the format argument) and puts each line in a list.*
+def ass_to_dict(data, format):
+    dict_data = []
+    for i in range(len(data)):
+        if data[i].startswith('Dialogue:'):
+            #send the line to the function remove_brackets
+            data[i] = remove_brackets(data[i])
+            line = data[i].split(',')
+            line.pop(0)
+            line = [x.strip() for x in line]
+            dict_data.append(dict(zip(format, line)))
+    return dict_data
+
+data_original = read_file('temp/video1.mkv_clean.mkv_sub.txt')
+data = data_original.copy()
+data = clean_data(data)
+
+sub = delete_garbage(data)
+sub = clean_data(sub)
+format = format_ass(sub)
+sub = ass_to_dict(sub, format)
+
+#print sub lign by lign.
+for i in range(len(sub)):
+    print(sub[i])
