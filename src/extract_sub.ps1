d09408a5cc291061e40672e293c1a7a7c79010c8
naliliV2 <nalili0000007@gmail.com>
2022-04-21 02:32:46 +0200

    :arrows_clockwise: Change for a better name

--- src/extract_sub.ps1
+++ src/extract_sub.ps1
@@ -1,13 +1,10 @@
 #make a list of all mkv files in the todo directory
 $list = @() # decalre the list
-(Get-ChildItem -Path 'temp').Name | ForEach-Object{
+(Get-ChildItem -Path 'temp').BaseName | ForEach-Object{
     $list += $_ # fill the list for each gci output
 }
 
 #Start list
 foreach($i in $list) {
-    dep/mkvextract.exe temp/$i tracks 1:temp/audio.ogg -c utf-8 2:temp/$($i)_sub.txt
+    dep/mkvextract.exe temp/$($i).mkv tracks -c utf-8 2:temp/$($i)_sub.txt
 }
-
-#Remove songs because it's ussless
-Remove-Item temp/audio.ogg
