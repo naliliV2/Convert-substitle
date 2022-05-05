637e002c4c25fb95a63561a1f43b4d8b2ca742db
naliliV2 <nalili0000007@gmail.com>
2022-05-05 11:24:34 +0200

    :heavy_plus_sign: Add an optional arg
    Added an optional arg for treat one by one every video (take less stockage)

--- src/clean_mkv.ps1
+++ src/clean_mkv.ps1
@@ -25,21 +25,36 @@ Param(
         HelpMessage="Enter the folder where the files will go.")] 
     [Alias("output", "o")] 
     [string[]]
-    $output_dir
+    $output_dir,
+
+    [parameter(
+        HelpMessage="Set True if input is a file, False or null if input is a directory.")]
+    [Alias("each", "e")]
+    [bool]
+    $one_file = $False
 )
 
 #make a list of all mkv files in the $input_dir
-$list_video = @() # decalre the list
-(Get-ChildItem -Path $input_dir ).BaseName | ForEach-Object{
-    $list_video += $_ # fill the list for each gci output
+if ($one_file -eq $False -or $one_file -eq $null) {
+    $list_video = @() # decalre the list
+    (Get-ChildItem -Path $input_dir ).BaseName | ForEach-Object{
+        $list_video += $_ # fill the list for each gci output
+    }
+
+    $nb = 0
+    foreach ($i in $list_video) {
+        #make a percent and round it to 2 decimals
+        $percent = [Math]::round(($nb/$list_video.Count)*100,2)
+
+        Write-Progress -Activity "Cleaning the video" -Status "$percent% Complete" -PercentComplete $percent 
+        dep/mkclean.exe "$($input_dir)\$($i).mkv" "$($output_dir)\$($i)_clean.mkv" >$null 2>&1
+        $nb++
+    }
 }
 
-$nb = 0
-foreach ($i in $list_video) {
-    #make a percent and round it to 2 decimals
-    $percent = [Math]::round(($nb/$list_video.Count)*100,2)
-
-    Write-Progress -Activity "Cleaning the video" -Status "$percent% Complete" -PercentComplete $percent 
-    dep/mkclean.exe "$($input_dir)\$($i).mkv" "$($output_dir)\$($i)_clean.mkv" >$null 2>&1
-    $nb++
-} 
+elseif ($one_file -eq $True) {
+    dep/mkclean.exe "$($input_dir)" "$($output_dir)\$((Get-ChildItem -File $input_dir).BaseName)_clean.mkv" >$null 2>&1
+}
+else {
+    Write-Error "The parameter 'one file' is not a boolean."
+}
--- start.ps1
+++ start.ps1
@@ -7,6 +7,9 @@ This file allows you to manage everything
 This is the directory of input all .mkv for extract the subtitle
 .PARAMETER output
 This is the directory of output all .mkv merge with .srt
+.PARAMETER Each
+Todo
+
 .NOTES
     Author:  nalili
     Version: Pre-Alpha 2.0
@@ -24,7 +27,14 @@ Param(
         HelpMessage="Enter the folder where the files will go.")] 
     [Alias("output", "o")] 
     [string[]]
-    $output_dir
+    $output_dir,
+
+    [Parameter(
+        HelpMessage="Define if you want one by one or no (default is : not)"
+    )]
+    [Alias("each", "one")]
+    [bool]
+    $one_by_one = $false
 )
 
 #Verify if $output is empty, set it to $input (directory where the files are)
@@ -39,15 +49,39 @@ function New-TemporaryDirectory {
     New-Item -ItemType Directory -Path (Join-Path $parent $name)
 }
 
-$TempDir = New-TemporaryDirectory
+if($one_by_one -eq $false -or $one_by_one -eq $null) 
+{
+    $TempDir = New-TemporaryDirectory
 
-src/clean_mkv.ps1 -dir $input_dir -output $TempDir
-src/extract_sub.ps1 -dir $TempDir -output $TempDir
+    src/clean_mkv.ps1 -dir $input_dir -output $TempDir
+    src/extract_sub.ps1 -dir $TempDir -output $TempDir
+    
+    py ./src/exctract_sub_ass/main.py $TempDir 
+    
+    src/merge_mkv.ps1 -dir $TempDir -output $output_dir
+    
+    Remove-Item $TempDir -Recurse -Force
+}
+elseif ($one_by_one -eq $true)
+{
+    $files = Get-ChildItem -Path $input_dir -Recurse -Filter "*.mkv"   
+    foreach ($file in $files)
+    {
+        $TempDir = New-TemporaryDirectory
 
-py ./src/exctract_sub_ass/main.py $TempDir 
-
-src/merge_mkv.ps1 -dir $TempDir -output $output_dir
-
-Remove-Item $TempDir -Recurse -Force
+        src/clean_mkv.ps1 -dir $file -output $TempDir -each $true
+        src/extract_sub.ps1 -dir $TempDir -output $TempDir
+        
+        py ./src/exctract_sub_ass/main.py $TempDir 
+        
+        src/merge_mkv.ps1 -dir $TempDir -output $output_dir
+        
+        Remove-Item $TempDir -Recurse -Force
+    }
+}
+else
+{
+    Write-Host "Error : $one_by_one is not a boolean"
+}
 
 Write-output "Completed"
